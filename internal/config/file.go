f3ec10e22c1826bf0c13aa5966e1d09ac4c50828
Adam Chalkley <atc0005@users.noreply.github.com>
2021-11-09 05:49:21 -0600

    Ignore false-positive gosec G307 linting errors
    Issues reported after upgrading golangci-lint to v1.43.0.
    gosec was updated in that version from v2.8.1 to v2.9.1.
    
    refs atc0005/query-meta#61
    refs golangci/golangci-lint#2299

--- internal/config/file.go
+++ internal/config/file.go
@@ -27,6 +27,9 @@ func (c *Config) loadConfigFile(configFile string) error {
 		return err
 	}
 
+	// #nosec G307
+	// Believed to be a false-positive from recent gosec release
+	// https://github.com/securego/gosec/issues/714
 	defer func() {
 		if err := fh.Close(); err != nil {
 			// Ignore "file already closed" errors
